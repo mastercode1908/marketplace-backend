d001ebb8275f8111f0f8a8d5e5fe4e95bbc0c3d7
Chu The Duc <chutheduc20@gmail.com>
2025-12-05 00:10:37 +0700

    fix BE

--- src/main/java/com/group7/marketplacesystem/catalog/dto/request/ProductCreateRequest.java
+++ src/main/java/com/group7/marketplacesystem/catalog/dto/request/ProductCreateRequest.java
@@ -1,8 +1,6 @@
 package com.group7.marketplacesystem.catalog.dto.request;
 
-import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,7 +8,6 @@ import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.util.List;
-
 
 @Data
 @AllArgsConstructor
@@ -21,23 +18,30 @@ public class ProductCreateRequest {
     @NotNull
     private Integer categoryId;
 
-    @NotBlank
+    @NotBlank(message = "Tên sản phẩm không được để trống")
+    @Size(min = 1, max = 255, message = "Tên sản phẩm không được vượt quá 255 ký tự")
     private String name;
 
+    @Size(min = 10, max = 2000, message = "Mô tả sản phẩm không được vượt quá 2000 ký tự")
     private String description;
 
-    @NotNull
-    @DecimalMin("0.0")
+    @NotNull(message = "Giá sản phẩm không được để trống")
+    @DecimalMin(value = "0", message = "Giá sản phẩm phải lớn hơn hoặc bằng 0")
+    @DecimalMax(value = "1000000000", message = "Giá sản phẩm quá lớn")
     private BigDecimal price;
 
     @NotNull
+    @DecimalMin(value = "0", message = "Giá sản phẩm phải lớn hơn hoặc bằng 0")
+    @DecimalMax(value = "100000", message = "Giá sản phẩm quá lớn")
     private Integer weight; // gram
 
-    private Integer stockQuantity = 0;
+    @NotNull(message = "Số lượng tồn kho không được để trống")
+    @Min(value = 0, message = "Số lượng tồn kho không được âm")
+    @Max(value = 100000, message = "Số lượng tồn kho quá lớn")
+    private Integer stockQuantity;
 
     private String productStatus = "Pending";
 
     private List<ProductMediaRequest> media;
-
 
 }
